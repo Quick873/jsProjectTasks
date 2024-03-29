b569724aca51802bd78471506e2b063d1c5d0f08
Quick873 <116125610+Quick873@users.noreply.github.com>
2024-03-28 20:07:12 -0500

    Arrays added

--- OneDrive/Desktop/Coding/jsProjectTask/script.js
+++ OneDrive/Desktop/Coding/jsProjectTask/script.js
@@ -1,1 +1,10 @@
 console.log("Hello, Everyone!")
+
+const greeting = ["Hello!", "Greetings!", "Hey!", "Welcome!"];
+
+const verse = ["John 3:16: For God so loved the world, that He gave His only begotten Son, that who believes in Him shall not perish but have everlasting life.",
+"2 Corinthians 5:21: God made Him who had no sin to be sin for us, so that in Him we might become the righteousness of God.",
+"Romans 12:10: Be devoted to one another in love. Honor one another above yourselves.",
+"Matthew 5:14: You are the light of the world. A town built on a hill cannot be hidden."];
+
+const blessing = ["God bless.", "With love", "Amen", "Make more and better disciples."];
