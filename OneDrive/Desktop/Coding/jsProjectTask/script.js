231fc1bd808cbf61ce8fc7d1a122a285209babd8
Quick873 <116125610+Quick873@users.noreply.github.com>
2024-03-29 21:27:10 -0500

    Working script!

--- OneDrive/Desktop/Coding/jsProjectTask/script.js
+++ OneDrive/Desktop/Coding/jsProjectTask/script.js
@@ -8,3 +8,17 @@ const verse = ["John 3:16: For God so loved the world, that He gave His only beg
 "Matthew 5:14: You are the light of the world. A town built on a hill cannot be hidden."];
 
 const blessing = ["God bless.", "With love", "Amen", "Make more and better disciples."];
+
+function getRandom(min, max) {
+    return Math.floor(Math.random() * (max -min)) + min;
+}
+
+var random = getRandom(0, 3);
+
+var theGreeting = greeting[random];
+var theVerse = verse[random];
+var theBlessing = blessing[random];
+
+console.log(theGreeting)
+console.log(theVerse)
+console.log(theBlessing)
